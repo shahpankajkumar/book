ef9e6592fffaf453dc7907658e52c58b1a10dabc
shah pankaj <spankaj769819@gmail.com>
2024-12-08 15:21:16 +0530

    URL Change for deployment

--- frontend/src/utils/actionURLs.js
+++ frontend/src/utils/actionURLs.js
@@ -1,8 +1,11 @@
-const USER_REGISTRATION = `${(process.env.REACT_APP_API_URL || 'https://book-server-ruby.vercel.app')}/user/register`;
-const USER_LOGIN = `${(process.env.REACT_APP_API_URL || 'https://book-server-ruby.vercel.app')}/user/login`;
-const GET_BOOK = `${(process.env.REACT_APP_API_URL || 'https://book-server-ruby.vercel.app')}/books/get`;
-const ADD_BOOK = `${(process.env.REACT_APP_API_URL || 'https://book-server-ruby.vercel.app')}/books/add`;
-const DELETE_BOOK = `${(process.env.REACT_APP_API_URL || 'https://book-server-ruby.vercel.app')}/books/delete`;
+const API = 'https://book-server-ruby.vercel.app/';
+// const API = 'process.env.REACT_APP_API_URL';
+
+const USER_REGISTRATION = `${API}/user/register`;
+const USER_LOGIN = `${API}/user/login`;
+const GET_BOOK = `${API}/books/get`;
+const ADD_BOOK = `${API}/books/add`;
+const DELETE_BOOK = `${API}/books/delete`;
 
 module.exports = {
     USER_REGISTRATION, USER_LOGIN, GET_BOOK, ADD_BOOK, DELETE_BOOK
