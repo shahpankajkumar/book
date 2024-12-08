f6c8150f306a65d5909360b75ae7e7474be709d4
shah pankaj <spankaj769819@gmail.com>
2024-12-08 15:14:03 +0530

    url issue fixed.

--- frontend/src/utils/actionURLs.js
+++ frontend/src/utils/actionURLs.js
@@ -1,8 +1,8 @@
-const USER_REGISTRATION = `${process.env.REACT_APP_API_URL}/user/register`;
-const USER_LOGIN = `${process.env.REACT_APP_API_URL}/user/login`;
-const GET_BOOK = `${process.env.REACT_APP_API_URL}/books/get`;
-const ADD_BOOK = `${process.env.REACT_APP_API_URL}/books/add`;
-const DELETE_BOOK = `${process.env.REACT_APP_API_URL}/books/delete`;
+const USER_REGISTRATION = `${(process.env.REACT_APP_API_URL || 'https://book-server-ruby.vercel.app')}/user/register`;
+const USER_LOGIN = `${(process.env.REACT_APP_API_URL || 'https://book-server-ruby.vercel.app')}/user/login`;
+const GET_BOOK = `${(process.env.REACT_APP_API_URL || 'https://book-server-ruby.vercel.app')}/books/get`;
+const ADD_BOOK = `${(process.env.REACT_APP_API_URL || 'https://book-server-ruby.vercel.app')}/books/add`;
+const DELETE_BOOK = `${(process.env.REACT_APP_API_URL || 'https://book-server-ruby.vercel.app')}/books/delete`;
 
 module.exports = {
     USER_REGISTRATION, USER_LOGIN, GET_BOOK, ADD_BOOK, DELETE_BOOK
